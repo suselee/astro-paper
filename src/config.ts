6a8d0abb006cba78bb4e3d1a23d4642a73eac087
suselee <lyhgreat@gmail.com>
2024-11-29 10:06:15 +0800

    test logo

--- src/config.ts
+++ src/config.ts
@@ -27,8 +27,8 @@ export const LOCALE = {
 export const LOGO_IMAGE = {
   enable: true,
   svg: false,
-  width: 216,
-  height: 46,
+  width: 100,
+  height: 23,
 };
 
 export const SOCIALS: SocialObjects = [
